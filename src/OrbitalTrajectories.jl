74f86b4dc3b423ff29201698014f5b6ba4117308
Dan Padilha <dan.padilha@ac.jaxa.jp>
2021-03-20 15:25:15 +0900

    Update support of GeneralizedGenerated to 0.3.2

--- src/OrbitalTrajectories.jl
+++ src/OrbitalTrajectories.jl
@@ -1,14 +1,6 @@
 module OrbitalTrajectories
 
     using Reexport
-    using GeneralizedGenerated: NGG
-
-    function __init__()
-        # XXX: Ensure that NGG is initialized appropriately, so that
-        # GeneralizedGenerated functions work properly. See the NGG_modules constant.
-        map(NGG.module_index, NGG_modules)
-        nothing
-    end
 
     #----------#
     # INCLUDES #
@@ -23,11 +15,5 @@ module OrbitalTrajectories
     #----------------#
     @reexport using OrbitalTrajectories.Dynamics
     @reexport using OrbitalTrajectories.SpiceUtils
-
-    #-----------#
-    # CONSTANTS #
-    #-----------#
-    # Output of this gives the order that we should touch module_index() in __init__
-    const NGG_modules = NGG._modules
 
 end # module
